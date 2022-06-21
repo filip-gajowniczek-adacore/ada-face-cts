9ddf84479a16030db32745a1e10dbe738b8126d2
Albert Lee <lee@adacore.com>
2022-06-21 00:18:47 -0400

    Make this a little bit less hard-coded.  Even though it still is.

--- workspace/sample-safetyext/compiler_intrinsics/gen-fake_gcc_symbols.sh
+++ workspace/sample-safetyext/compiler_intrinsics/gen-fake_gcc_symbols.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-gcc_lib_dir="/opt/GNAT/2017-GPL/lib/gcc/x86_64-pc-linux-gnu/6.3.1"
+gnat_install_dir="/opt/GNAT/2017-GPL"
+platform="x86_64-pc-linux-gnu"
+version=$( /bin/ls -1 "$gnat_install_dir/lib/gcc/$platform" | tail -1 )
+gcc_lib_dir="$gnat_install_dir/lib/gcc/$platform/$version"
 output_file=fake_gcc_symbols.c
 
 function list_defined_symbols () {
