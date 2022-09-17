6ae4ef12eb294621319997d993346ebc96499fbb
Filip Gajowniczek <gajowniczek@adacore.com>
2022-09-17 15:41:37 -0500

    Fix for Conformance verification compile test for disallowed features in stubbed runtime that are caught with pragma Restrictions

--- workspace/AdaUoC/src/demo.adb
+++ workspace/AdaUoC/src/demo.adb
@@ -20,10 +20,10 @@ procedure Demo is
    --  and the GNAT specific restrictions:
    --       pragma Restrictions (No_Use_Of_Entity => Wide_String);
    --       pragma Restrictions (No_Use_Of_Entity => Wide_Wide_String);
-   X : Wide_Character;
-   Y : constant Wide_Character :=
-   Ada.Characters.Handling.To_Wide_Character ('x');
-   Z : Wide_Character := Ada.Strings.Wide_Space;
+   --  X : Wide_Character;
+   --  Y : constant Wide_Character :=
+   --  Ada.Characters.Handling.To_Wide_Character ('x');
+   --  Z : Wide_Character := Ada.Strings.Wide_Space;
 
 
    -- Disallowed POSIX API usage
